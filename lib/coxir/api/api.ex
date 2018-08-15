75c0cc8baa35fc81e3d247cfdf242c701338d45d
Santi T <santitm99@gmail.com>
2018-08-16 01:18:12 +0200

    Typo

--- lib/coxir/api/api.ex
+++ lib/coxir/api/api.ex
@@ -115,7 +115,7 @@ defmodule Coxir.API do
     |> case do
       [route, param] when param in @major_parameters ->
         cond do
-          String.contains?(route, "messages") and method == :delete ->
+          String.contains?(path, "messages") and method == :delete ->
             "#{route}/messages:#{method}"
           true ->
             route
