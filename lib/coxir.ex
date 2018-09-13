eeb6d41daf381747e6921c4e60d8ca9bc2aff2db
Juan <20613820+PurgePJ@users.noreply.github.com>
2018-09-13 11:31:43 +0200

    Update coxir.ex

--- lib/coxir.ex
+++ lib/coxir.ex
@@ -22,7 +22,6 @@ defmodule Coxir do
 
   @doc false
   def start(_type, _args) do
-    :erlang.system_flag(:fullsweep_after, 0)
     children = [
       supervisor(Voice, []),
       supervisor(Stage, []),
