539e9ad809f04ce9110e735edce4683071424c60
Juan <priciel@protonmail.com>
2018-09-16 20:04:43 +0200

    Print removed

--- lib/coxir/cache/ttl.ex
+++ lib/coxir/cache/ttl.ex
@@ -2,17 +2,20 @@ defmodule Coxir.Cache.TTL do
   @behaviour Coxir.Cache.Cache
 
   def insert(cache, data) do
-    "#{cache}s" |> String.to_atom
+    "#{cache}s"
+    |> String.to_atom()
     |> SimpleTTL.put(data)
   end
+
   def get(cache, data) do
-    "#{cache}s" |> String.to_atom
+    "#{cache}s"
+    |> String.to_atom()
     |> SimpleTTL.get(data)
   end
+
   def delete(cache, data) do
-    IO.inspect(data) 
-    "#{cache}s" |> String.to_atom
+    "#{cache}s"
+    |> String.to_atom()
     |> SimpleTTL.delete(data)
   end
-
 end
