96e04f56309b3a21eeb4b5ea5c8646f5b85e720b
satom99 <santitm99@gmail.com>
2020-05-14 23:17:50 +0200

    Snowflake

---
+++ lib/coxir/snowflake.ex
@@ -0,0 +1,30 @@
+defmodule Coxir.Snowflake do
+  @moduledoc """
+  Work in progress.
+  """
+  use Ecto.Type
+
+  defguard is_snowflake(term) when is_integer(term)
+
+  def type do
+    :string
+  end
+
+  def cast(value) when is_binary(value) do
+    case Integer.parse(value) do
+      {integer, ""} ->
+        cast(integer)
+
+      _other ->
+        :error
+    end
+  end
+
+  def cast(value) when is_snowflake(value) do
+    {:ok, value}
+  end
+
+  def cast(_value) do
+    :error
+  end
+end
