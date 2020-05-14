cc89448a9be326528bb383b50c32ef4f3abf6b79
satom99 <santitm99@gmail.com>
2020-05-14 23:46:15 +0200

    Update user.ex

--- lib/coxir/models/user.ex
+++ lib/coxir/models/user.ex
@@ -8,5 +8,7 @@ defmodule Coxir.Model.User do
     field(:username, :string)
     field(:discriminator, :string)
     field(:avatar, :string)
+
+    has_many(:guilds, Guild, foreign_key: :owner_id)
   end
 end
