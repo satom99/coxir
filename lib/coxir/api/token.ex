f6a61bd780ef6d8c1cc0f18ea815a9d7a967692b
Santi T <santitm99@gmail.com>
2021-04-13 01:11:21 +0200

    Rename variable

--- lib/coxir/api/token.ex
+++ lib/coxir/api/token.ex
@@ -6,9 +6,9 @@ defmodule Coxir.API.Token do
 
   alias Tesla.Middleware.Headers
 
-  def call(%{opts: options} = env, next, _options) do
+  def call(%{opts: opts} = env, next, _options) do
     config = Application.get_env(:coxir, :token)
-    token = Keyword.get(options, :token, config)
+    token = Keyword.get(opts, :token, config)
 
     headers = [{"Authorization", "Bot #{token}"}]
 
