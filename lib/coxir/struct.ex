dceb040aeab3f623a2476fd615f08afd23fb2471
Santi T <santitm99@gmail.com>
2021-04-13 21:00:27 +0200

    Model.changeset/1 and Model.changeset/2

--- lib/coxir/struct.ex
+++ lib/coxir/struct.ex
@@ -8,11 +8,36 @@ defmodule Coxir.Struct do
     quote location: :keep do
       use Ecto.Schema
 
+      import Ecto.Changeset
+
       alias Coxir.Snowflake
       alias Coxir.{User, Guild, Channel}
 
       @primary_key {:id, Snowflake, []}
       @foreign_key_type Snowflake
+
+      def changeset(params) do
+        __MODULE__
+        |> struct
+        |> changeset(params)
+      end
+
+      def changeset(struct, params) do
+        fields = __schema__(:fields)
+        changeset = cast(struct, params, fields)
+
+        associations = __schema__(:associations)
+
+        Enum.reduce(
+          associations,
+          changeset,
+          fn name, changeset ->
+            cast_assoc(changeset, name)
+          end
+        )
+      end
+
+      defoverridable changeset: 2
     end
   end
 end
