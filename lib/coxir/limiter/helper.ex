cef5950cff1e3af5a113509d9bfb6c6df135eb8d
Santi T <santitm99@gmail.com>
2021-04-15 13:51:08 +0200

    Introduce some offset noise

--- lib/coxir/limiter/helper.ex
+++ lib/coxir/limiter/helper.ex
@@ -8,14 +8,18 @@ defmodule Coxir.Limiter.Helper do
     DateTime.to_unix(DateTime.utc_now(), :millisecond)
   end
 
-  def offset_now(offset) do
-    time_now() + offset
+  def offset_now(offset \\ 0) do
+    time_now() + offset + offset_noise()
   end
 
   def wait_hit(bucket) do
     with {:error, timeout} <- Limiter.hit(bucket) do
-      Process.sleep(timeout)
+      Process.sleep(timeout + offset_noise())
       wait_hit(bucket)
     end
+  end
+
+  defp offset_noise do
+    trunc(:rand.uniform() * 500)
   end
 end
