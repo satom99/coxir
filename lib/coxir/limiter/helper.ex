081a3de808548360d4704da5ede64e2a77348f74
Santi T <santitm99@gmail.com>
2021-04-15 12:56:50 +0200

    Limiter.Helper.wait_hit/1

--- lib/coxir/limiter/helper.ex
+++ lib/coxir/limiter/helper.ex
@@ -2,11 +2,20 @@ defmodule Coxir.Limiter.Helper do
   @moduledoc """
   Common helper functions for `Coxir.Limiter` implementations.
   """
+  alias Coxir.Limiter
+
   def time_now do
     DateTime.to_unix(DateTime.utc_now(), :millisecond)
   end
 
   def offset_now(offset) do
     time_now() + offset
+  end
+
+  def wait_hit(bucket) do
+    with {:error, timeout} <- Limiter.hit(bucket) do
+      Process.sleep(timeout)
+      wait_hit(bucket)
+    end
   end
 end
