28a498dbd6abf14acdaaf344acc3d866e831de0c
Santi T <santitm99@gmail.com>
2021-04-16 13:27:38 +0200

    Handle embedded associations in Model.Loader.load/2

--- lib/coxir/model/loader.ex
+++ lib/coxir/model/loader.ex
@@ -9,13 +9,9 @@ defmodule Coxir.Model.Loader do
 
   @spec load(Model.name(), map) :: Model.object()
   def load(model, object) do
-    fields = model.__schema__(:fields)
-
     model
     |> struct()
-    |> cast(object, fields)
-    |> apply_changes()
-    |> Storage.put()
+    |> loader(object)
   end
 
   @spec preload(Model.object(), atom, keyword) :: Model.object()
@@ -23,6 +19,36 @@ defmodule Coxir.Model.Loader do
     reflection = model.__schema__(:association, association)
     force = Keyword.get(options, :force, false)
     preloader(reflection, struct, force)
+  end
+
+  defp loader(%model{} = struct, object) do
+    fields = model.__schema__(:fields)
+    associations = model.__schema__(:associations)
+
+    struct
+    |> cast(object, fields)
+    |> associer(associations)
+    |> apply_changes()
+    |> Storage.put()
+  end
+
+  defp associer(%{data: struct} = changeset, [name | associations]) do
+    struct = preload(struct, name, force: true)
+
+    changeset
+    |> Map.put(:data, struct)
+    |> cast_assoc(name, with: &associer/2)
+    |> associer(associations)
+  end
+
+  defp associer(changeset, []) do
+    changeset
+  end
+
+  defp associer(%_model{} = struct, object) do
+    struct
+    |> loader(object)
+    |> change
   end
 
   defp preloader(%{field: field} = reflection, struct, false) do
