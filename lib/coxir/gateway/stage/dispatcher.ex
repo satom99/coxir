a9a16160baa648e0b0f420f16cfe8548f7f287a9
Santi T <santitm99@gmail.com>
2021-04-20 13:36:19 +0200

    Implement basic Coxir.Gateway.Dispatcher

--- lib/coxir/gateway/stage/dispatcher.ex
+++ lib/coxir/gateway/stage/dispatcher.ex
@@ -2,4 +2,17 @@ defmodule Coxir.Gateway.Dispatcher do
   @moduledoc """
   Work in progress.
   """
+  use GenStage
+
+  def start_link(producer) do
+    GenStage.start_link(__MODULE__, producer)
+  end
+
+  def init(producer) do
+    {:producer_consumer, nil, subscribe_to: [producer]}
+  end
+
+  def handle_events(events, _from, state) do
+    {:noreply, events, state}
+  end
 end
