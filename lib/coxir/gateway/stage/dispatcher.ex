aa71b6b77cce904c565393e4a86e07624ae8e4e7
Santi T <santitm99@gmail.com>
2021-04-20 14:50:19 +0200

    Possible Dispatcher implementation

--- lib/coxir/gateway/stage/dispatcher.ex
+++ lib/coxir/gateway/stage/dispatcher.ex
@@ -4,6 +4,9 @@ defmodule Coxir.Gateway.Dispatcher do
   """
   use GenStage
 
+  alias Coxir.Model.Loader
+  alias Coxir.Message
+
   def start_link(producer) do
     GenStage.start_link(__MODULE__, producer)
   end
@@ -13,6 +16,36 @@ defmodule Coxir.Gateway.Dispatcher do
   end
 
   def handle_events(events, _from, state) do
+    events =
+      Enum.map(
+        events,
+        fn {name, object} ->
+          object = sanitize(object)
+          handle_event(name, object)
+        end
+      )
+
     {:noreply, events, state}
+  end
+
+  defp handle_event(:MESSAGE_CREATE, object) do
+    message = Loader.load(Message, object)
+    {:MESSAGE_CREATE, message}
+  end
+
+  defp handle_event(name, object) do
+    {name, object}
+  end
+
+  defp sanitize(object) when is_map(object) do
+    Map.new(object, &sanitize/1)
+  end
+
+  defp sanitize({key, value}) do
+    {to_string(key), sanitize(value)}
+  end
+
+  defp sanitize(term) do
+    term
   end
 end
