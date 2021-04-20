3e48c2a0681fd39f72f7e41276888071d5756d36
Santi T <santitm99@gmail.com>
2021-04-20 13:58:39 +0200

    Implement Coxir.Gateway.Consumer

--- lib/coxir/gateway/stage/consumer.ex
+++ lib/coxir/gateway/stage/consumer.ex
@@ -2,4 +2,27 @@ defmodule Coxir.Gateway.Consumer do
   @moduledoc """
   Work in progress.
   """
+  use ConsumerSupervisor
+
+  defstruct [
+    :dispatchers,
+    :module
+  ]
+
+  alias __MODULE__
+
+  def start_link(state) do
+    ConsumerSupervisor.start_link(__MODULE__, state)
+  end
+
+  def init(%Consumer{dispatchers: dispatchers, module: module}) do
+    children = [module]
+
+    options = [
+      strategy: :one_for_one,
+      subscribe_to: dispatchers
+    ]
+
+    ConsumerSupervisor.init(children, options)
+  end
 end
