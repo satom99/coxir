bbbf72caea92228e731649b1c3c860122e91abdd
Santi T <santitm99@gmail.com>
2021-04-22 01:14:53 +0200

    Introduce Coxir.Integration

---
+++ lib/coxir/model/entities/integration.ex
@@ -0,0 +1,23 @@
+defmodule Coxir.Integration do
+  @moduledoc """
+  Work in progress.
+  """
+  use Coxir.Model
+
+  embedded_schema do
+    field(:name, :string)
+    field(:type, :string)
+    field(:enabled, :boolean)
+    field(:syncing, :boolean)
+    field(:enable_emoticons, :boolean)
+    field(:expire_behavior, :integer)
+    field(:expire_grace_period, :integer)
+    field(:synced_at, :utc_datetime)
+    field(:subscriber_count, :integer)
+    field(:revoked, :boolean)
+
+    belongs_to(:guild, Guild, primary_key: true)
+    belongs_to(:role, Role)
+    belongs_to(:user, User)
+  end
+end
