6cfe3059353cf252fce754419c5ce5e076b97ca9
Santi T <santitm99@gmail.com>
2021-04-22 13:53:07 +0200

    Basic Coxir.Gateway.Sharder.Default

--- lib/coxir/gateway/sharder/default.ex
+++ lib/coxir/gateway/sharder/default.ex
@@ -2,4 +2,19 @@ defmodule Coxir.Gateway.Sharder.Default do
   @moduledoc """
   Work in progress.
   """
+  use Supervisor
+
+  def start_link(options) do
+    Supervisor.start_link(__MODULE__, options)
+  end
+
+  def init(_options) do
+    children = []
+
+    options = [
+      strategy: :one_for_one
+    ]
+
+    Supervisor.init(children, options)
+  end
 end
