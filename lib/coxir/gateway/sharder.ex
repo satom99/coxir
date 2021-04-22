c39c61e7807219fc20bce938dbc14b5608c3f31c
Santi T <santitm99@gmail.com>
2021-04-22 22:15:37 +0200

    Begin Gateway.Sharder

--- lib/coxir/gateway/sharder.ex
+++ lib/coxir/gateway/sharder.ex
@@ -2,6 +2,11 @@ defmodule Coxir.Gateway.Sharder do
   @moduledoc """
   Work in progress.
   """
+  defstruct [
+    :shard_count,
+    :session_options
+  ]
+
   @type t :: module
 
   @callback child_spec(term) :: Supervisor.child_spec()
