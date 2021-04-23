5b9f5ae4619320b80f45d81ce5337f0ca017511d
Santi T <santitm99@gmail.com>
2021-04-23 14:37:32 +0200

    Lower the offset

--- lib/coxir/limiter/default.ex
+++ lib/coxir/limiter/default.ex
@@ -74,7 +74,7 @@ defmodule Coxir.Limiter.Default do
         ],
         [
           {
-            {:"$1", :"$2", offset_now(5000)}
+            {:"$1", :"$2", offset_now(1000)}
           }
         ]
       }
