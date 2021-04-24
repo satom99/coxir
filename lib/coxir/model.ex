6d9995b36f0f0d54ef950ae62a8d5e38d5d00ae7
Santi T <santitm99@gmail.com>
2021-04-24 23:26:57 +0200

    Default Model.fetch/2 to nil

--- lib/coxir/model.ex
+++ lib/coxir/model.ex
@@ -40,11 +40,15 @@ defmodule Coxir.Model do
         Loader.get(__MODULE__, key, options)
       end
 
+      def fetch(_key, _options) do
+        nil
+      end
+
       def preload(struct, association, options \\ []) do
         Loader.preload(struct, association, options)
       end
 
-      defoverridable(preload: 3)
+      defoverridable(fetch: 2, preload: 3)
     end
   end
 end
