8a2b5ce4b7c2f4ce5b6fb43e7099476cb27eba5a
Santi T <santitm99@gmail.com>
2021-04-26 23:47:12 +0200

    Introduce API.Error

---
+++ lib/coxir/api/error.ex
@@ -0,0 +1,23 @@
+defmodule Coxir.API.Error do
+  @moduledoc """
+  Work in progress.
+  """
+  use Ecto.Schema
+
+  import Ecto.Changeset
+
+  @type t :: %__MODULE__{}
+
+  embedded_schema do
+    field(:code, :integer)
+    field(:message, :string)
+  end
+
+  def cast(object) do
+    fields = __schema__(:fields)
+
+    %__MODULE__{}
+    |> cast(object, fields)
+    |> apply_changes()
+  end
+end
