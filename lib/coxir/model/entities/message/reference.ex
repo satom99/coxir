420e1cdff20a2b78e359c2c73bbdd2c1db62d795
Santi T <santitm99@gmail.com>
2021-04-29 14:16:37 +0200

    Typespec

--- lib/coxir/model/entities/message/reference.ex
+++ lib/coxir/model/entities/message/reference.ex
@@ -8,6 +8,8 @@ defmodule Coxir.Message.Reference do
 
   @primary_key false
 
+  @type t :: %__MODULE__{}
+
   embedded_schema do
     belongs_to(:message, Message, primary_key: true)
     belongs_to(:channel, Channel, primary_key: true)
