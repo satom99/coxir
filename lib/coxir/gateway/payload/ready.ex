56d8727560164ed093579bc111866d1bd30d9d0a
Santi T <santitm99@gmail.com>
2021-04-30 01:00:06 +0200

    Uncomment embeds

--- lib/coxir/gateway/payload/ready.ex
+++ lib/coxir/gateway/payload/ready.ex
@@ -4,14 +4,12 @@ defmodule Coxir.Gateway.Payload.Ready do
   """
   use Coxir.Gateway.Payload
 
-  # alias Coxir.{User, Guild}
-
   embedded_schema do
     field(:v, :integer)
     field(:session_id, :string)
     field(:shard, {:array, :integer})
 
-    # embeds_one(:user, User)
-    # embeds_many(:guilds, Guild)
+    embeds_one(:user, User)
+    embeds_many(:guilds, Guild)
   end
 end
