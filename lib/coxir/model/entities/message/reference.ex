5f5666be91f83084f95bac62dca99f80ab97e0ae
Santi T <santitm99@gmail.com>
2021-04-29 14:20:15 +0200

    Undocument unused methods

--- lib/coxir/model/entities/message/reference.ex
+++ lib/coxir/model/entities/message/reference.ex
@@ -11,4 +11,16 @@ defmodule Coxir.Message.Reference do
     belongs_to(:channel, Channel, primary_key: true)
     belongs_to(:guild, Guild)
   end
+
+  @doc false
+  def get(key, options)
+
+  @doc false
+  def create(params, options)
+
+  @doc false
+  def update(struct, params, options)
+
+  @doc false
+  def delete(struct, options)
 end
