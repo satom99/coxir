01aef2766cffc803b1f7094d3f2b01e84e4b426b
Santi T <santitm99@gmail.com>
2021-04-29 23:09:09 +0200

    Update config.exs

--- example/config/config.exs
+++ example/config/config.exs
@@ -1,9 +1,3 @@
 import Config
 
-config :coxir,
-  storage: Coxir.Storage.Default,
-  limiter: Coxir.Limiter.Default,
-  sharder: Coxir.Gateway.Sharder.Default,
-  shard_count: 1,
-  intents: :non_privileged,
-  token: ""
+config :coxir, :token, ""
