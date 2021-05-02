fb3b5377dd16f2d8f65615b5c2bec2f843d708e3
Santi T <santitm99@gmail.com>
2021-05-03 00:16:50 +0200

    Base Voice.Instance

--- lib/coxir/voice/instance.ex
+++ lib/coxir/voice/instance.ex
@@ -2,4 +2,23 @@ defmodule Coxir.Voice.Instance do
   @moduledoc """
   Work in progress.
   """
+  use Supervisor
+
+  alias Coxir.Voice.Session
+
+  def start_link(session_options) do
+    Supervisor.start_link(__MODULE__, session_options)
+  end
+
+  def init(session_options) do
+    children = [
+      {Session, session_options}
+    ]
+
+    options = [
+      strategy: :rest_for_one
+    ]
+
+    Supervisor.init(children, options)
+  end
 end
