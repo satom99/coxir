a19eea5e1f2046d163404d80538b1cf70c798e33
Santi T <santitm99@gmail.com>
2021-05-02 15:39:40 +0200

    Allow passing an :as option to API calls

--- lib/coxir/api/helper.ex
+++ lib/coxir/api/helper.ex
@@ -2,10 +2,19 @@ defmodule Coxir.API.Helper do
   @moduledoc """
   Common helper functions for `Coxir.API` middlewares.
   """
-  alias Coxir.Token
+  alias Tesla.Env
+  alias Coxir.{Token, Gateway}
 
-  @spec get_token(Tesla.Env.t()) :: Token.t()
-  def get_token(%{opts: options}) do
+  @spec get_token(Env.t()) :: Token.t()
+  def get_token(%Env{opts: options}) do
+    obtain_token(options)
+  end
+
+  defp obtain_token(%{as: gateway}) do
+    Gateway.get_token(gateway)
+  end
+
+  defp obtain_token(options) do
     config = Application.get_env(:coxir, :token)
     Keyword.get(options, :token, config)
   end
