58d3dabdad7906d4c21d42230915c77930cc94cc
Santi T <santitm99@gmail.com>
2021-05-05 20:10:30 +0200

    Update select_protocol.ex

--- lib/coxir/voice/payload/select_protocol.ex
+++ lib/coxir/voice/payload/select_protocol.ex
@@ -4,6 +4,21 @@ defmodule Coxir.Voice.Payload.SelectProtocol do
   """
   use Coxir.Voice.Payload
 
+  alias Coxir.Voice.Payload.SelectProtocol.Data
+
+  embedded_schema do
+    field(:protocol, :string, default: "udp")
+
+    embeds_one(:data, Data)
+  end
+end
+
+defmodule Coxir.Voice.Payload.SelectProtocol.Data do
+  @moduledoc """
+  Work in progress.
+  """
+  use Coxir.Voice.Payload
+
   embedded_schema do
     field(:address, :string)
     field(:port, :integer)
