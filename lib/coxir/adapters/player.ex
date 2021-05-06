846da344cfffd180e7e978f50976ea88fa467bea
Santi T <santitm99@gmail.com>
2021-05-06 19:41:42 +0200

    Adapter behaviour

--- lib/coxir/adapters/player.ex
+++ lib/coxir/adapters/player.ex
@@ -2,4 +2,25 @@ defmodule Coxir.Player do
   @moduledoc """
   Work in progress.
   """
+  defstruct [
+    :udp_socket,
+    :remote_ip,
+    :remote_port
+  ]
+
+  @type t :: %__MODULE__{}
+
+  @type player :: pid | GenServer.name()
+
+  @type playable :: any
+
+  @callback child_spec(term) :: GenServer.child_spec()
+
+  @callback update(player, t) :: :ok
+
+  @callback play(player, playable) :: :ok
+
+  @callback pause(player) :: :ok
+
+  @callback resume(player) :: :ok
 end
