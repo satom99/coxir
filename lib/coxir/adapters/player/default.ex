53579527729df18b4d4f015c78d0bf790fe8ba9f
Santi T <santitm99@gmail.com>
2021-05-07 00:06:36 +0200

    Blank Player.Default

--- lib/coxir/adapters/player/default.ex
+++ lib/coxir/adapters/player/default.ex
@@ -2,4 +2,21 @@ defmodule Coxir.Player.Default do
   @moduledoc """
   Work in progress.
   """
+  use GenServer
+
+  def ready(_player, _audio) do
+    :ok
+  end
+
+  def invalidate(_player) do
+    :ok
+  end
+
+  def start_link(state) do
+    GenServer.start_link(__MODULE__, state)
+  end
+
+  def init(state) do
+    {:ok, state}
+  end
 end
