2ae273de96641696e2e662a3ce98f91bc45431d1
Santi T <santitm99@gmail.com>
2021-05-08 13:22:40 +0200

    New logic

--- lib/coxir/voice/instance.ex
+++ lib/coxir/voice/instance.ex
@@ -12,6 +12,8 @@ defmodule Coxir.Voice.Instance do
 
   @type instance :: pid
 
+  @update_session {:continue, :update_session}
+
   defstruct [
     :player_module,
     :player,
@@ -26,8 +28,6 @@ defmodule Coxir.Voice.Instance do
     :session,
     :audio
   ]
-
-  @update_session {:continue, :update_session}
 
   def has_endpoint?(instance) do
     GenServer.call(instance, :has_endpoint?)
@@ -105,8 +105,10 @@ defmodule Coxir.Voice.Instance do
   end
 
   def handle_continue(:update_session, %Instance{session: session} = state) do
-    Process.exit(session, :restart)
-    {:noreply, state}
+    state = %{state | session: nil, audio: nil}
+    update_player(state)
+    Process.exit(session, :kill)
+    {:noreply, state, @update_session}
   end
 
   def handle_call(:has_endpoint?, _from, %Instance{endpoint_host: endpoint_host} = state) do
@@ -121,22 +123,23 @@ defmodule Coxir.Voice.Instance do
   def handle_call(
         {:play, player_module, playable, options},
         _from,
-        %Instance{player_module: player_module} = state
+        %Instance{player: nil} = state
       ) do
-    %{player: player} = state = update_player(state)
+    start_argument = {playable, options}
+    child_spec = player_module.child_spec(start_argument)
 
-    result = player_module.play(player, playable, options)
+    %{start: start_mfa} = child_spec
+    {module, function, arguments} = start_mfa
 
-    {:reply, result, state}
-  end
+    case apply(module, function, arguments) do
+      {:ok, player} ->
+        state = %{state | player_module: player_module, player: player}
+        update_player(state)
+        {:reply, :ok, state}
 
-  def handle_call(
-        {:play, player_module, _playable, _options} = call,
-        from,
-        %Instance{player_module: nil} = state
-      ) do
-    state = %{state | player_module: player_module}
-    handle_call(call, from, state)
+      {:error, _reason} = result ->
+        {:reply, result, state}
+    end
   end
 
   def handle_call(
@@ -145,39 +148,48 @@ defmodule Coxir.Voice.Instance do
         %Instance{player: player} = state
       ) do
     Process.exit(player, :kill)
-    state = %{state | player_module: nil, player: nil}
+    state = %{state | player: nil}
     handle_call(call, from, state)
   end
 
   def handle_call(:pause, _from, %Instance{player: nil} = state) do
-    {:reply, :no_player, state}
+    {:reply, {:error, :no_player}, state}
   end
 
   def handle_call(:pause, _from, %Instance{player_module: player_module, player: player} = state) do
-    result = player_module.pause(player)
+    result =
+      if player_module.playing?(player) do
+        player_module.pause(player)
+      else
+        {:error, :already_paused}
+      end
+
     {:reply, result, state}
   end
 
   def handle_call(:resume, _from, %Instance{player: nil} = state) do
-    {:reply, :no_player, state}
+    {:reply, {:error, :no_player}, state}
   end
 
   def handle_call(:resume, _from, %Instance{player_module: player_module, player: player} = state) do
-    result = player_module.resume(player)
+    result =
+      if not player_module.playing?(player) do
+        player_module.resume(player)
+      else
+        {:error, :already_resumed}
+      end
+
     {:reply, result, state}
   end
 
   def handle_call(:stop_playing, _from, %Instance{player: nil} = state) do
-    {:reply, :no_player, state}
+    {:reply, :ok, state}
   end
 
-  def handle_call(
-        :stop_playing,
-        _from,
-        %Instance{player_module: player_module, player: player} = state
-      ) do
-    result = player_module.stop_playing(player)
-    {:reply, result, state}
+  def handle_call(:stop_playing, _from, %Instance{player: player} = state) do
+    Process.exit(player, :kill)
+    state = %{state | player: nil}
+    {:reply, :ok, state}
   end
 
   def handle_cast({:update, struct}, state) do
@@ -191,7 +203,7 @@ defmodule Coxir.Voice.Instance do
 
   def handle_info({:EXIT, session, :normal}, %Instance{session: session} = state) do
     state = %{state | session_id: nil, endpoint_host: nil, session: nil, audio: nil}
-    state = update_player(state)
+    update_player(state)
 
     %Instance{gateway: gateway, guild_id: guild_id, channel_id: channel_id} = state
     Voice.update_voice_state(gateway, guild_id, channel_id)
@@ -199,23 +211,15 @@ defmodule Coxir.Voice.Instance do
     {:noreply, state}
   end
 
-  def handle_info({:EXIT, session, :restart}, %Instance{session: session} = state) do
-    state = %{state | session: nil, audio: nil}
-    state = update_player(state)
-    {:noreply, state, @update_session}
-  end
-
   def handle_info({:EXIT, session, :stop}, %Instance{session: session} = state) do
     {:stop, :normal, state}
   end
 
-  def handle_info({:EXIT, player, :killed}, %Instance{player: player} = state) do
-    state = %{state | player: nil}
-    state = update_player(state)
+  def handle_info({:EXIT, player, :normal}, %Instance{player: player} = state) do
     {:noreply, state}
   end
 
-  def handle_info({:EXIT, _pid, _reason}, state) do
+  def handle_info({:EXIT, _process, :killed}, state) do
     {:noreply, state}
   end
 
@@ -252,40 +256,19 @@ defmodule Coxir.Voice.Instance do
     }
 
     state = %{state | audio: audio}
-    state = update_player(state)
+    update_player(state)
     {:noreply, state}
   end
 
-  defp update_player(%Instance{player_module: nil} = state) do
-    state
+  defp update_player(%Instance{player: nil}) do
+    :noop
   end
 
-  defp update_player(%Instance{player_module: player_module, audio: nil} = state) do
-    %{player: player} = state = ensure_player(state)
-
+  defp update_player(%Instance{player_module: player_module, player: player, audio: nil}) do
     player_module.invalidate(player)
-
-    state
   end
 
-  defp update_player(%Instance{player_module: player_module, audio: audio} = state) do
-    %{player: player} = state = ensure_player(state)
-
+  defp update_player(%Instance{player_module: player_module, player: player, audio: audio}) do
     player_module.ready(player, audio)
-
-    state
-  end
-
-  defp ensure_player(%Instance{player_module: player_module, player: nil} = state) do
-    %{start: start_mfa} = player_module.child_spec([])
-    {module, function, arguments} = start_mfa
-
-    {:ok, player} = apply(module, function, arguments)
-
-    %{state | player: player}
-  end
-
-  defp ensure_player(state) do
-    state
   end
 end
