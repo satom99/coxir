9e243e393778a4aedd9e715a8a45d1f00df996ff
Santi T <santitm99@gmail.com>
2021-05-11 00:01:21 +0200

    Update error.ex

--- lib/coxir/api/error.ex
+++ lib/coxir/api/error.ex
@@ -16,6 +16,10 @@ defmodule Coxir.API.Error do
     %Error{status: status, code: code, message: message}
   end
 
+  def cast(status, _term) do
+    %Error{status: status}
+  end
+
   def message(%Error{status: status, code: nil}) do
     reason = :httpd_util.reason_phrase(status)
     "(#{status}) #{reason}"
