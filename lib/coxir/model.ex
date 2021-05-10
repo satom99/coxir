c1b2bdfcd5bd219e2afc920b1491541ada25800d
Santi T <santitm99@gmail.com>
2021-05-10 23:48:43 +0200

    Change :stop to :killed

--- lib/coxir/model.ex
+++ lib/coxir/model.ex
@@ -24,7 +24,7 @@ defmodule Coxir.Model do
 
   @callback storable?() :: boolean
 
-  @callback get(key, Loader.options()) :: instance | nil
+  @callback get(key, Loader.options()) :: instance | nil | Error.t()
 
   @callback preload(instance, Loader.preloads(), Loader.options()) :: instance
 
