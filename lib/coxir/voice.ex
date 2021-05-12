1f970fa17cdacf766a5ed4a564b13fe925a46fd4
Santi T <santitm99@gmail.com>
2021-05-12 20:11:34 +0200

    Documentation for Voice

--- lib/coxir/voice.ex
+++ lib/coxir/voice.ex
@@ -1,6 +1,6 @@
 defmodule Coxir.Voice do
   @moduledoc """
-  Work in progress.
+  Allows for interaction with Discord voice channels.
   """
   use Supervisor
 
@@ -12,7 +12,33 @@ defmodule Coxir.Voice do
   alias Coxir.Player
   alias __MODULE__
 
-  @spec join(Channel.t(), keyword) :: Instance.instance()
+  @typedoc """
+  The options that must be passed to `join/2`.
+  """
+  @type join_options :: [
+          as: Gateway.gateway(),
+          self_deaf: :boolean | false,
+          self_mute: :boolean | false
+        ]
+
+  @typedoc """
+  The options that can be passed to `play/3`.
+  """
+  @type play_options :: [player: Player.t() | Player.Default] | Player.options()
+
+  @typedoc """
+  The options that must be passed to `leave/2`.
+  """
+  @type leave_options :: [as: Gateway.gateway()]
+
+  @doc """
+  Joins a given voice channel.
+
+  If the user is already in the channel, the function acts as a no-op.
+
+  If the user is in a different channel of the same guild, it will switch and stop playing.
+  """
+  @spec join(Channel.t(), join_options) :: Instance.instance()
   def join(%Channel{id: channel_id, guild_id: guild_id}, options) do
     gateway = Keyword.fetch!(options, :as)
     user_id = Gateway.get_user_id(gateway)
@@ -32,33 +58,55 @@ defmodule Coxir.Voice do
     instance
   end
 
-  @spec play(Instance.instance(), Player.playable(), keyword) :: :ok | {:error, term}
+  @doc """
+  Begins playing audio on a given instance.
+
+  Refer to the documentation of the player in use for more information on the arguments.
+
+  If no custom player is provided, the default `Coxir.Player.Default` will be used.
+  """
+  @spec play(Instance.instance(), Player.playable(), play_options) :: :ok | {:error, term}
   def play(instance, playable, options \\ []) do
     player_module = Keyword.get(options, :player, Player.Default)
     Instance.play(instance, player_module, playable, options)
   end
 
+  @doc """
+  Pauses audio playback on a given instance.
+  """
   @spec pause(Instance.instance()) :: :ok | {:error, :no_player}
   def pause(instance) do
     Instance.pause(instance)
   end
 
+  @doc """
+  Resumes audio playback on a given instance.
+  """
   @spec resume(Instance.instance()) :: :ok | {:error, :no_player}
   def resume(instance) do
     Instance.resume(instance)
   end
 
+  @doc """
+  Returns whether audio is currently playing on a given instance.
+  """
   @spec playing?(Instance.instance()) :: boolean
   def playing?(instance) do
     Instance.playing?(instance)
   end
 
+  @doc """
+  Stops playing audio on a given instance.
+  """
   @spec stop_playing(Instance.instance()) :: :ok
   def stop_playing(instance) do
     Instance.stop_playing(instance)
   end
 
-  @spec leave(Guild.t() | Channel.t(), keyword) :: :ok
+  @doc """
+  Leaves from a given voice channel, or the active voice channel for a guild.
+  """
+  @spec leave(Guild.t() | Channel.t(), leave_options) :: :ok
   def leave(%Guild{id: guild_id}, options) do
     channel = %Channel{guild_id: guild_id}
     leave(channel, options)
@@ -72,6 +120,7 @@ defmodule Coxir.Voice do
     update_voice_state(gateway, guild_id, nil)
   end
 
+  @doc false
   def update_voice_state(gateway, guild_id, channel_id, options \\ []) do
     channel = %Channel{id: channel_id, guild_id: guild_id}
     session = Gateway.get_shard(gateway, channel)
@@ -87,6 +136,7 @@ defmodule Coxir.Voice do
     Session.update_voice_state(session, update_voice_state)
   end
 
+  @doc false
   def update(_gateway, user_id, guild_id, %VoiceState{channel_id: nil}) do
     terminate_instance(user_id, guild_id)
   end
@@ -97,10 +147,12 @@ defmodule Coxir.Voice do
     |> Instance.update(gateway, struct)
   end
 
+  @doc false
   def start_link(state) do
     Supervisor.start_link(__MODULE__, state, name: Voice)
   end
 
+  @doc false
   def init(_state) do
     Supervisor.init([], strategy: :one_for_one)
   end
