7734e356c7f27066913412f2d4e9a8f11bb05817
Santi T <santitm99@gmail.com>
2021-05-12 21:13:37 +0200

    Update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Coxir.MixProject do
   def project do
     [
       app: :coxir,
-      version: "0.1.0",
+      version: "2.0.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
