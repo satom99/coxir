fd2d81808f189c2c828f6561394851b41c7d4888
Santi T <santitm99@gmail.com>
2021-05-13 21:39:56 +0200

    Update voice_instance_update.ex

--- lib/coxir/gateway/payload/voice_instance_update.ex
+++ lib/coxir/gateway/payload/voice_instance_update.ex
@@ -2,13 +2,22 @@ defmodule Coxir.Gateway.Payload.VoiceInstanceUpdate do
   @moduledoc """
   Work in progress.
   """
+  alias Coxir.Model.Snowflake
   alias Coxir.Voice.Instance
 
   @type t :: %__MODULE__{
-          instance: Instance.instance()
+          instance: Instance.instance(),
+          guild_id: Snowflake.t() | nil,
+          channel_id: Snowflake.t(),
+          invalid?: boolean,
+          playing?: boolean
         }
 
   defstruct [
-    :instance
+    :instance,
+    :guild_id,
+    :channel_id,
+    {:invalid?, false},
+    {:playing?, false}
   ]
 end
