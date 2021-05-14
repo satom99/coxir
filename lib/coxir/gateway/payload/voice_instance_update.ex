862640df0c34ecc4d1c2e33b159f24fce2604643
Santi T <santitm99@gmail.com>
2021-05-14 12:01:13 +0200

    Add :has_player?

--- lib/coxir/gateway/payload/voice_instance_update.ex
+++ lib/coxir/gateway/payload/voice_instance_update.ex
@@ -10,6 +10,7 @@ defmodule Coxir.Gateway.Payload.VoiceInstanceUpdate do
           user_id: Snowflake.t(),
           guild_id: Snowflake.t() | nil,
           channel_id: Snowflake.t(),
+          has_player?: boolean,
           invalid?: boolean,
           playing?: boolean
         }
@@ -19,6 +20,7 @@ defmodule Coxir.Gateway.Payload.VoiceInstanceUpdate do
     :user_id,
     :guild_id,
     :channel_id,
+    :has_player?,
     :invalid?,
     :playing?
   ]
