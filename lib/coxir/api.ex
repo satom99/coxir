359f7ec08541852267d16d8e51dcc5785480d01d
Santi T <santitm99@gmail.com>
2021-05-14 12:41:43 +0200

    Document token usage

--- lib/coxir/api.ex
+++ lib/coxir/api.ex
@@ -10,6 +10,10 @@ defmodule Coxir.API do
 
   @typedoc """
   The options that can be passed to `perform/4`.
+
+  If the `:as` option is present, the token of the given gateway will be used.
+
+  If no token is provided, one is expected to be configured as `:token` under the `:coxir` app.
   """
   @type options :: [
           token: Token.t() | none,
