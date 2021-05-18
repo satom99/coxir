689679d98eeb082ec6ea1773379d774700a0d7fd
Santi T <santitm99@gmail.com>
2021-05-18 20:50:11 +0200

    No need for this to be an association

--- lib/coxir/gateway/payload/request_guild_members.ex
+++ lib/coxir/gateway/payload/request_guild_members.ex
@@ -5,12 +5,11 @@ defmodule Coxir.Gateway.Payload.RequestGuildMembers do
   use Coxir.Gateway.Payload
 
   embedded_schema do
+    field(:guild_id, Snowflake)
     field(:query, :string)
     field(:limit, :integer)
     field(:presences, :boolean)
     field(:user_ids, {:array, Snowflake})
     field(:nonce, :string)
-
-    belongs_to(:guild, Guild)
   end
 end
